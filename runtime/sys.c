d524ddb070cf720f7bc68f92536c0178eb00b0c5
Antonin Décimo <antonin@tarides.com>
2024-08-27 16:17:46 +0200

    Provide a system(3) replacement on Android

--- runtime/sys.c
+++ runtime/sys.c
@@ -505,6 +505,51 @@ void caml_sys_init(char_os * exe_name, char_os **argv)
 #endif
 #endif
 
+#ifdef __ANDROID__
+/* Android 8 Oreo and later seem to block system(3). Provide a
+ * replacement, taken from POSIX.1-2024.
+ * https://pubs.opengroup.org/onlinepubs/9799919799/functions/system.html */
+int caml_sys_system(const char *cmd)
+{
+  int stat;
+  pid_t pid;
+  struct sigaction sa, savintr, savequit;
+  sigset_t saveblock;
+  if (cmd == NULL)
+    return(1);
+  sa.sa_handler = SIG_IGN;
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = 0;
+  sigemptyset(&savintr.sa_mask);
+  sigemptyset(&savequit.sa_mask);
+  sigaction(SIGINT, &sa, &savintr);
+  sigaction(SIGQUIT, &sa, &savequit);
+  sigaddset(&sa.sa_mask, SIGCHLD);
+  pthread_sigmask(SIG_BLOCK, &sa.sa_mask, &saveblock);
+  if ((pid = fork()) == 0) {
+    sigaction(SIGINT, &savintr, (struct sigaction *)0);
+    sigaction(SIGQUIT, &savequit, (struct sigaction *)0);
+    sigprocmask(SIG_SETMASK, &saveblock, (sigset_t *)0);
+    execl("/bin/sh", "sh", "-c", "--", cmd, (char *)0);
+    _exit(127);
+  }
+  if (pid == -1) {
+    stat = -1; /* errno comes from fork() */
+  } else {
+    while (waitpid(pid, &stat, 0) == -1) {
+      if (errno != EINTR){
+        stat = -1;
+        break;
+      }
+    }
+  }
+  sigaction(SIGINT, &savintr, (struct sigaction *)0);
+  sigaction(SIGQUIT, &savequit, (struct sigaction *)0);
+  sigprocmask(SIG_SETMASK, &saveblock, (sigset_t *)0);
+  return(stat);
+}
+#endif
+
 #ifdef HAS_SYSTEM
 CAMLprim value caml_sys_system_command(value command)
 {
