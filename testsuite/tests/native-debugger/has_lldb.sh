47cb55c058cbe2bd4e2887c199d777771978439b
Antonin Décimo <antonin@tarides.com>
2025-04-15 07:30:31 +0200

    tests: native-debugger: fix shellcheck warnings
    Don't redeclare variables that ocamltest always provides in scripts.

--- testsuite/tests/native-debugger/has_lldb.sh
+++ testsuite/tests/native-debugger/has_lldb.sh
@@ -7,9 +7,9 @@ version () {
     echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }'
 }
 
-if ! which lldb > /dev/null 2>&1; then
-    echo "lldb not available" > "${ocamltest_response}"
-    exit ${TEST_SKIP}
+if ! command -v lldb > /dev/null 2>&1; then
+    echo "lldb not available" > "${ocamltest_response:?}"
+    exit "${TEST_SKIP}"
 else
     if [ "$OCAML_OS" = "macos" ]; then
         # macOS version
@@ -17,21 +17,21 @@ else
         # We need XCode 15.3 or greater
         # lldb-1500.0.404.7
         # Apple Swift version 5.10 (swiftlang-5.10.0.13 clang-1500.3.9.4)
-        if [ $(version "$LLDB_VERSION") -ge $(version "1500.0.404.7") ]; then
-            exit ${TEST_PASS}
+        if [ "$(version "$LLDB_VERSION")" -ge "$(version "1500.0.404.7")" ]; then
+            exit "${TEST_PASS}"
         else
-            exit ${TEST_SKIP}
+            exit "${TEST_SKIP}"
         fi
     elif [ "$OCAML_OS" = "linux" ]; then
         # Linux version
         LLDB_VERSION=$(lldb --version |awk -F' ' '{print $3}')
-        if [ $(version "$LLDB_VERSION") -ge $(version "14.0.0") ]; then
-            exit ${TEST_PASS}
+        if [ "$(version "$LLDB_VERSION")" -ge "$(version "14.0.0")" ]; then
+            exit "${TEST_PASS}"
         else
-            exit ${TEST_SKIP}
+            exit "${TEST_SKIP}"
         fi
     else
-        exit ${TEST_SKIP}
+        exit "${TEST_SKIP}"
     fi
 
 
--- testsuite/tests/native-debugger/sanitize.sh
+++ testsuite/tests/native-debugger/sanitize.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
-test_source_directory="$1"
-test_build_directory="$2"
-ocamltest_response="$3"
-test_name="$4"
+test_name="$1"
 
-awk -f ${test_source_directory}/sanitize.awk \
-    ${test_build_directory}/${test_name}.opt.output > ${test_build_directory}/${test_name}.opt.awk.output
+awk -f "${test_source_directory:?}/sanitize.awk" \
+    "${test_build_directory}/${test_name}.opt.output" > "${test_build_directory:?}/${test_name}.opt.awk.output"
 
-mv ${test_build_directory}/${test_name}.opt.output ${test_build_directory}/${test_name}.opt.output.bak
-cp ${test_build_directory}/${test_name}.opt.awk.output ${test_build_directory}/${test_name}.opt.output
+mv "${test_build_directory}/${test_name}.opt.output" "${test_build_directory}/${test_name}.opt.output.bak"
+cp "${test_build_directory}/${test_name}.opt.awk.output" "${test_build_directory}/${test_name}.opt.output"
