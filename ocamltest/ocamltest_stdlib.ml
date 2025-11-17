1258af4f64fc5c62667311ad40f1ba72ab94e783
Antonin Décimo <antonin@tarides.com>
2025-11-17 12:24:03 +0100

    ocamltest: retry deleting response files
    In some runs on Windows, ocamltest is unable to delete response
    files. Perhaps all processes haven't yet closed them. Try deleting
    them 3 times. If it still fails, mark the files to be deleted on next
    reboot (Windows doesn't have a tmpfs).

--- ocamltest/ocamltest_stdlib.ml
+++ ocamltest/ocamltest_stdlib.ml
@@ -214,8 +214,14 @@ module Sys = struct
     in
     List.iter cp_dir subdirs
 
+  external mark_for_deletion : string -> bool = "caml_mark_for_deletion"
+
   let force_remove file =
-    if file_exists file then remove file
+    let rec retry = function
+      | 0 -> (try remove file with _ -> ignore (mark_for_deletion file))
+      | n -> try remove file with _ -> retry (n-1)
+    in
+    if file_exists file then retry 3
 
   let with_chdir path f =
     let oldcwd = Sys.getcwd () in
