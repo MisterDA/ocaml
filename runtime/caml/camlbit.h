2fc059bacb2de3728aa10668f6bf24b3d4f14d79
Antonin Décimo <antonin.decimo@gmail.com>
2026-08-17 23:18:01 +0200

    caml/camlbit.h

---
+++ runtime/caml/camlbit.h
@@ -0,0 +1,24 @@
+/**************************************************************************/
+/*                                                                        */
+/*                                 OCaml                                  */
+/*                                                                        */
+/*   All rights reserved.  This file is distributed under the terms of    */
+/*   the GNU Lesser General Public License version 2.1, with the          */
+/*   special exception on linking described in the file LICENSE.          */
+/*                                                                        */
+/**************************************************************************/
+
+#ifndef CAML_STDBIT_H
+#define CAML_STDBIT_H
+
+#ifdef CAML_INTERNALS
+
+#include "config.h"
+#include <stdint.h>
+
+#ifdef HAVE_STDBIT_H
+#include <stdbit.h>
+#endif
+
+#endif /* CAML_INTERNALS */
+#endif /* CAML_STDBIT_H */
