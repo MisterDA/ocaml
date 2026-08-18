63d1e84f508755907d2a9361afc160fe74f62d6e
Antonin Décimo <antonin.decimo@gmail.com>
2026-08-18 12:13:21 +0200

    Remove Reverse_N macros from caml/reverse.h

--- runtime/caml/reverse.h
+++ runtime/caml/reverse.h
@@ -20,35 +20,6 @@
 
 #ifdef CAML_INTERNALS
 
-#define Reverse_16(dst,src) {                                               \
-  char * _p;                                                                \
-  char _a, _b;                                                              \
-  _p = (char *) (src);                                                      \
-  _a = _p[0]; _b = _p[1];                                                   \
-  _p = (char *) (dst);                                                      \
-  _p[0] = _b; _p[1] = _a;                                                   \
-}
-
-#define Reverse_32(dst,src) {                                               \
-  char * _p;                                                                \
-  char _a, _b, _c, _d;                                                      \
-  _p = (char *) (src);                                                      \
-  _a = _p[0]; _b = _p[1]; _c = _p[2]; _d = _p[3];                           \
-  _p = (char *) (dst);                                                      \
-  _p[0] = _d; _p[1] = _c; _p[2] = _b; _p[3] = _a;                           \
-}
-
-#define Reverse_64(dst,src) {                                               \
-  char * _p;                                                                \
-  char _a, _b, _c, _d, _e, _f, _g, _h;                                      \
-  _p = (char *) (src);                                                      \
-  _a = _p[0]; _b = _p[1]; _c = _p[2]; _d = _p[3];                           \
-  _e = _p[4]; _f = _p[5]; _g = _p[6]; _h = _p[7];                           \
-  _p = (char *) (dst);                                                      \
-  _p[0] = _h; _p[1] = _g; _p[2] = _f; _p[3] = _e;                           \
-  _p[4] = _d; _p[5] = _c; _p[6] = _b; _p[7] = _a;                           \
-}
-
 #define Perm_index(perm,i) ((perm >> (i * 4)) & 0xF)
 
 #define Permute_64(dst,perm_dst,src,perm_src) {                             \
